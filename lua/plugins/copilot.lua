1978bd497a6fbfbb983b3c5f2e5177e984d79610
Loïc PILLER <loicpiller@mail.fr>
2025-04-26 00:48:30 +0200

    Update copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,23 +1,21 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	build = ":Copilot auth",
-	event = "BufReadPost",
-	opts = {
-		suggestion = {
-			enabled = not vim.g.ai_cmp,
-			auto_trigger = true,
-			hide_during_completion = vim.g.ai_cmp,
-			keymap = {
-				accept = false, -- handled by nvim-cmp / blink.cmp
-				next = "<M-]>",
-				prev = "<M-[>",
-			},
-		},
-		panel = { enabled = false },
-		filetypes = {
-			markdown = true,
-			help = true,
-		},
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false }, -- Disable natif system
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = { "copilot.lua" },
+		config = function()
+			require("copilot_cmp").setup()
+		end,
 	},
 }
